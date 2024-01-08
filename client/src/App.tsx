44a07c496fa19029bc855d152e9150d1f40494a1
alex <alexnbui@gmail.com>
2024-01-07 19:23:47 -0800

    Add media queries

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -47,19 +47,23 @@ export default function App() {
             <div className='w-full h-full flex items-center justify-center bg-black'>
                 <form className='text-white border-2 border-white rounded-xl flex flex-col gap-5
                 p-[25px] w-full m-[20px]
-                sm:p-[50px] sm:w-[700px]'
+                sm:p-[50px] sm:max-w-[700px]'
                     onSubmit={handleSubmit((data) => { reserve.mutate(transformData(data)) })}>
                     <div className='flex gap-5'>
                         <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" placeholder='First Name' {...register("firstName")} />
                         <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" placeholder='Last Name' {...register("lastName")} />
                     </div>
                     <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" placeholder='Confirmation Number' {...register("confirmation")} />
-                    <div className='flex gap-5'>
-                        <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("month")} />
-                        <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("day")} />
-                        <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("year")} />
-                        <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("hour")} />
-                        <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("minute")} />
+                    <div className='flex gap-5 flex-col sm:flex-row'>
+                        <div className='flex gap-5'>
+                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("month")} />
+                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("day")} />
+                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("year")} />
+                        </div>
+                        <div className='flex gap-5'>
+                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("hour")} />
+                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("minute")} />
+                        </div>
                     </div>
                     <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" placeholder='Email' {...register("email")} />
                     <div className='flex justify-between'>
@@ -67,7 +71,7 @@ export default function App() {
                         {reserve.isError ? <div className='text-red-500 my-auto'>An error occured contacting the server.</div> : null}
                         {reserve.isPending ? <div className='text-yellow-500 my-auto'>Loading...</div> : null}
                         {reserve.isIdle ? <div /> : null}
-                        <button className='p-3 bg-neutral-800 rounded-md hover:bg-neutral-700 transition' type="submit">Auto Reserve</button>
+                        <button className='p-3 bg-neutral-800 rounded-md active:bg-neutral-700 sm:hover:bg-neutral-700 transition' type="submit">Auto Reserve</button>
                     </div>
                 </form>
             </div>
