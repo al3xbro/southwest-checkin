8434cbe7c68bb41410e9dcff029770cf30400ddf
alex <alexnbui@gmail.com>
2024-01-08 23:10:10 -0800

    Add input mask for date and time

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,16 @@
 import axios from 'axios'
-import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+import { FieldErrors, useForm } from 'react-hook-form'
 import { useMutation } from '@tanstack/react-query'
 
 type ReserveFormData = {
     firstName: string,
     lastName: string,
     confirmation: string,
-    month: number,
-    day: number,
-    year: number,
-    hour: number,
-    minute: number,
+    date: string,
+    time: string,
     email: string,
+    meridiem: 'pm' | 'am',
 }
 
 type ReserveRequestData = {
@@ -23,7 +22,8 @@ type ReserveRequestData = {
 }
 
 function transformData(data: ReserveFormData): ReserveRequestData {
-    const dateTimeString = new Date(data.year, data.month, data.day, data.hour, data.minute).toISOString()
+    console.log(data.date, data.time, data.meridiem)
+    const dateTimeString = new Date(/* TODO */).toISOString()
     return {
         firstName: data.firstName,
         lastName: data.lastName,
@@ -33,9 +33,28 @@ function transformData(data: ReserveFormData): ReserveRequestData {
     }
 }
 
+function displayErrors(errors: FieldErrors<ReserveFormData>) {
+    return errors.firstName?.message ?? errors.lastName?.message ?? errors.confirmation?.message ?? errors.email?.message ?? errors.date?.message ?? errors.time?.message ?? errors.meridiem?.message ?? ''
+}
+
+function normalizeDate(date: string) {
+    date = date.replace(/\D/g, '').slice(0, 8);
+    if (date.length >= 5) {
+        return `${date.slice(0, 2)}/${date.slice(2, 4)}/${date.slice(4)}`;
+    }
+    else if (date.length >= 3) {
+        return `${date.slice(0, 2)}/${date.slice(2)}`;
+    }
+    return date
+}
+
+function normalizeTime(time: string) {
+    return time.replace(/\D/g, '').match(/\d{1,2}/g)?.join(":").slice(0, 5) ?? ""
+}
+
 export default function App() {
 
-    const { register, handleSubmit, getValues } = useForm<ReserveFormData>()
+    const { register, handleSubmit, getValues, formState: { errors } } = useForm<ReserveFormData>()
     const reserve = useMutation({
         mutationFn: (data: ReserveRequestData) => {
             return axios.post('http://localhost:8000/reserve', data)
@@ -45,36 +64,57 @@ export default function App() {
     return (
         <>
             <div className='w-full h-full flex items-center justify-center bg-black'>
-                <form className='text-white border-2 border-white rounded-xl flex flex-col gap-5
-                p-[25px] w-full m-[20px]
-                sm:p-[50px] sm:max-w-[700px]'
-                    onSubmit={handleSubmit((data) => { reserve.mutate(transformData(data)) })}>
+                <form
+                    className='text-white border-2 border-white rounded-xl flex flex-col gap-5
+                    p-[25px] w-full m-[20px]
+                    sm:p-[50px] sm:max-w-[700px]'
+                    onSubmit={handleSubmit((data) => { reserve.mutate(transformData(data)) })}
+                >
                     <div className='flex gap-5'>
-                        <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" placeholder='First Name' {...register("firstName")} />
-                        <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" placeholder='Last Name' {...register("lastName")} />
+                        <input className='p-3 bg-neutral-800 rounded-md w-full' type='text' placeholder='First Name' {...register('firstName', { required: { value: true, message: 'First name required.' } })} />
+                        <input className='p-3 bg-neutral-800 rounded-md w-full' type='text' placeholder='Last Name' {...register('lastName', { required: { value: true, message: 'Last name required.' } })} />
                     </div>
-                    <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" placeholder='Confirmation Number' {...register("confirmation")} />
-                    <div className='flex gap-5 flex-col sm:flex-row'>
-                        <div className='flex gap-5'>
-                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("month")} />
-                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("day")} />
-                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("year")} />
+                    <input className='p-3 bg-neutral-800 rounded-md w-full' type='text' placeholder='Confirmation Number' {...register('confirmation', { required: { value: true, message: 'Confirmation number required.' } })} />
+                    <input className='p-3 bg-neutral-800 rounded-md w-full' type='text' placeholder='Email' inputMode='email' {...register('email', { required: { value: true, message: 'Email required.' } })} />
+
+                    {/* date */}
+                    <div className='flex flex-col sm:flex-row gap-5'>
+                        {/* input here */}
+                        <input className='p-3 bg-neutral-800 rounded-md w-full' type='text' inputMode='numeric' placeholder='mm/dd/yyyy' onChange={(e) => { e.target.value = normalizeDate(e.target.value) }}
+                            ref={register('date', {
+                                // required: {
+                                //     value: true,
+                                //     message: 'Valid date required.'
+                                // },
+
+                            }).ref}
+                        />
+                        <div className='flex gap-5 w-full'>
+                            <input className='p-3 bg-neutral-800 rounded-md w-full' type='text' inputMode='numeric' placeholder='hh:mm' onChange={(e) => { e.target.value = normalizeTime(e.target.value) }}
+                                ref={register('time', {
+                                    // required: {
+                                    //     value: true,
+                                    //     message: 'Valid time required.'
+                                    // },
+                                }).ref}
+                            />
+                            <select className='bg-neutral-800 rounded-md p-3' {...register('meridiem', { required: { value: true, message: 'Valid time required.' } })}>
+                                <option value='am'>AM</option>
+                                <option value='pm'>PM</option>
+                            </select>
                         </div>
-                        <div className='flex gap-5'>
-                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("hour")} />
-                            <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" {...register("minute")} />
-                        </div>
-                    </div>
-                    <input className='p-3 bg-neutral-800 rounded-md w-full' type="text" placeholder='Email' {...register("email")} />
-                    <div className='flex justify-between'>
+                    </div >
+
+                    {/* submit and status */}
+                    < div className='flex justify-between' >
                         {reserve.isSuccess ? <div className='text-green-500 my-auto'>Success! You will automatically be checked in at time here </div> : null}
                         {reserve.isError ? <div className='text-red-500 my-auto'>An error occured contacting the server.</div> : null}
                         {reserve.isPending ? <div className='text-yellow-500 my-auto'>Loading...</div> : null}
-                        {reserve.isIdle ? <div /> : null}
-                        <button className='p-3 bg-neutral-800 rounded-md active:bg-neutral-700 sm:hover:bg-neutral-700 transition' type="submit">Auto Reserve</button>
-                    </div>
-                </form>
-            </div>
+                        {reserve.isIdle ? <div className='text-red-500 my-auto'>{displayErrors(errors)}</div> : null}
+                        <button className='p-3 bg-neutral-800 rounded-md active:bg-neutral-700 sm:hover:bg-neutral-700 transition' type='submit'>Auto Reserve</button>
+                    </div >
+                </form >
+            </div >
         </>
     )
 }
