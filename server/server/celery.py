83b0012f88c716eb37c8cd7499b4fa35ebe36fe4
alex <alexnbui@gmail.com>
2024-01-14 00:40:13 -0800

    Add header fetch celery job 🥬

--- server/server/celery.py
+++ server/server/celery.py
@@ -5,3 +5,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 app = Celery("server")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "get-headers": {
+        "task": "server.tasks.get_headers",
+        "schedule": 86400,
+    },
+}
