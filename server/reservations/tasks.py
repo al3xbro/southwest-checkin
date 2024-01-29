c9930794ee1317c4b3a04bfa8ba62ba7af92f85c
Alex Bui <alexnbui@gmail.com>
2024-01-28 19:50:51 -0800

    Add debug messages

--- server/reservations/tasks.py
+++ server/reservations/tasks.py
@@ -40,45 +40,45 @@ scheduler = BackgroundScheduler({
 })
 
 # southwest API endpoint and URL; may change
-CHECKIN_URL = "https://mobile.southwest.com/check-in"
-API_ENDPOINT = "https://mobile.southwest.com/api/mobile-air-operations/v1/mobile-air-operations/page/check-in"
+HEADER_FETCH_URL = 'https://mobile.southwest.com/check-in'
+CHECKIN_URL = 'https://mobile.southwest.com/api/mobile-air-operations/v1/mobile-air-operations/page/check-in'
 # stolen from @byalextran at https://github.com/byalextran/southwest-headers
-HEADER_REGEX = "x-api-key|x-user-experience-id|x-channel-id|^[\w]+?-\w{1,2}$"
+HEADER_REGEX = 'x-api-key|x-user-experience-id|x-channel-id|^[\w]+?-\w{1,2}$'
 
 options = Options()
-options.add_argument("--headless")
+options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
-driver.get(CHECKIN_URL)
+driver.get(HEADER_FETCH_URL)
 
 
 @scheduler.scheduled_job('interval', hours=12)
 def get_headers():
 
-    print("Fetching headers...")
+    print('Fetching headers...')
 
     # start broswer
 
     # wait for page load
     wait = WebDriverWait(driver, 10)
     wait.until(
-        EC.presence_of_element_located((By.NAME, "recordLocator")) and
-        EC.presence_of_element_located((By.NAME, "firstName")) and
-        EC.presence_of_element_located((By.NAME, "lastName"))
+        EC.presence_of_element_located((By.NAME, 'recordLocator')) and
+        EC.presence_of_element_located((By.NAME, 'firstName')) and
+        EC.presence_of_element_located((By.NAME, 'lastName'))
     )
 
     # fill out form
-    confirmation_input = driver.find_element(By.NAME, "recordLocator")
-    first_name_input = driver.find_element(By.NAME, "firstName")
-    last_name_input = driver.find_element(By.NAME, "lastName")
+    confirmation_input = driver.find_element(By.NAME, 'recordLocator')
+    first_name_input = driver.find_element(By.NAME, 'firstName')
+    last_name_input = driver.find_element(By.NAME, 'lastName')
 
     confirmation_input.clear()
-    confirmation_input.send_keys("RANDRA")
+    confirmation_input.send_keys('RANDRA')
 
     first_name_input.clear()
-    first_name_input.send_keys("Raj")
+    first_name_input.send_keys('Raj')
 
     last_name_input.clear()
-    last_name_input.send_keys("Andra")
+    last_name_input.send_keys('Andra')
 
     confirmation_input.submit()
 
@@ -88,32 +88,33 @@ def get_headers():
     # grab headers
     headers = {}
     for request in driver.requests:
-        if request.response and request.url.startswith(API_ENDPOINT):
+        if request.response and request.url.startswith(CHECKIN_URL):
             for key in request.headers:
                 if re.match(HEADER_REGEX, key, re.I):
                     headers[key] = request.headers[key]
 
     # write to file
     if headers != {}:
-        f = open("headers.json", "w")
+        f = open('headers.json', 'w')
         json.dump(headers, f)
         driver.quit()
         f.close()
         driver.quit()
     else:
-        print("Error fetching headers. Retrying...")
+        print('Error fetching headers. Retrying...')
         get_headers()
 
-    print("Done fetching headers!")
+    print('Done fetching headers!')
 
 
 def process_reservation(first_name, last_name, confirmation_number, email):
-    with open("headers.json") as f:
+    with open('headers.json') as f:
         headers = json.load(f)
-        headers["Content-Type"] = "application/json"
-        res = requests.post(f"{API_ENDPOINT}/{confirmation_number}", headers=headers, json={
-            "firstName": first_name,
-            "lastName": last_name,
-            "recordLocator": confirmation_number,
+        headers['Content-Type'] = 'application/json'
+        res = requests.post(f'{CHECKIN_URL}/{confirmation_number}', headers=headers, json={
+            'firstName': first_name,
+            'lastName': last_name,
+            'recordLocator': confirmation_number,
         })
+        print(headers)
         print(res.json())
